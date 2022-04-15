aecfbaf1eb92251efade95726cd630a2b0730f84
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2022-04-15 04:29:24 -0400

    A website with movies
    1. Add tests
    2. Add a webpage with movies

---
+++ flaskr/movies.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request, render_template
+from flaskr.db import get_movies
+
+bp = Blueprint('movies', __name__, url_prefix='/movies')
+
+# https://werkzeug.palletsprojects.com/en/2.0.x/routing/
+# Rules that end with a slash are “branches”, others are “leaves”. 
+# If strict_slashes is enabled (the default), 
+# visiting a branch URL without a trailing slash will redirect to the URL with a slash appended.
+@bp.route('/', methods=['GET'], strict_slashes=False)
+def movie_list():
+    movies = get_movies()
+    return render_template('movies.html', movies=movies)
